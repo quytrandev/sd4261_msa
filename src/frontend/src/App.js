64893fc4129720aca81fcd987393e188449dd662
Quy Tran <quytrant@nashtechglobal.com>
2023-10-31 11:39:39 +0700

    Trigger CI & image updater

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -63,7 +63,7 @@ export default class App extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
-              <h1>Todos ArgoCD v2.0</h1>
+              <h1 style={{color: "green"}}>Todos ArgoCD v2.0 - Version 8</h1>
               <div className="todo-app">
                 <AddTodo handleAddTodo={this.handleAddTodo} />
                 <TodoList todos={this.state.todos} />
